0992ab2168ae333bd4e0835173dbee5e93f6ccc8
Mohammed ragab ali <regoo707@gmail.com>
2022-02-14 15:11:09 +0200

    Create install-docker-postgres.sh

---
+++ install-docker-postgres.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt install docker-ce -y
+docker run --name postgresql-container -p 5432:5432 -e POSTGRES_PASSWORD=P@ssw0rd -d postgres
