f3e951fcd13aeb4b02bd96069b8ad937ba1c21fc
Mohammed ragab ali <regoo707@gmail.com>
2022-02-14 15:35:28 +0200

    Update install-docker-postgres.sh

--- install-docker-postgres.sh
+++ install-docker-postgres.sh
@@ -5,4 +5,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 apt install docker-ce -y
-docker run --name postgresql-container -p 5432:5432 -e POSTGRES_PASSWORD=P@ssw0rd -d postgres
+docker run -d \
+    --name my-postgres \
+    --restart unless-stopped
+    -e POSTGRES_PASSWORD=P@ssw0rd \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+    -v /custom/mount:/var/lib/postgresql/data \
+    postgres
